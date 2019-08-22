5add86154fc826aecd552669bb45c0cfbdd41001
gccunanan <gccunanan@gmail.com>
2019-08-22 09:54:31 -0700

    added style to header component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar(){
+    var score = 0;
+    var topScore = 9000;
     return (
         <nav className="navbar">
             <ul><li class="brand"><a href="/">Hyperbolic Click Game</a></li>
             <li class="">Click an image to start training!</li>
-            <li>Score: 0 | Top Score: 12</li>
+            <li>Current Power Level: {score} | Highest Power Level: {topScore}</li>
             </ul>
         </nav>
     );
